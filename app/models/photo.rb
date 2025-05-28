fae20387cec34250d8a56840abb8fe0a7a4c1ee0
MCXZ123 <austin.zeng@outlook.com>
2025-05-28 00:21:40 +0000

    photos 1

---
+++ app/models/photo.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id             :bigint           not null, primary key
+#  caption        :string
+#  comments_count :integer
+#  likes_count    :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  owner_id       :integer
+#
+class Photo < ApplicationRecord
+  belongs_to :owner, class_name: "User"
+  has_one_attached :image
+
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+end
